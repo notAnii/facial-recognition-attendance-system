814fa12c3b78bd99d59760dbb40077ea24ed6bd7
notAnii <animazingtoho@gmail.com>
2023-01-06 00:50:29 +0400

    added response codes for get requests

--- backend/api/app.py
+++ backend/api/app.py
@@ -12,9 +12,10 @@ def login():
     # placeholder
         teacher_id = request.json['teacher_id']
         email = request.json['email']
+        password = request.json['password']
         return teacher_id, 200
     except Exception as e:
-        return e
+        return e, 404
 
 #starting the attendance 
 @app.route('/api/startRecognition', methods = ['GET'])
@@ -38,13 +39,13 @@ def get_single_student(student_id):
 @app.route('/api/teachers', methods = ['GET'])
 def get_all_teachers():
     result = all_teachers()
-    return jsonify(result)
+    return jsonify(result), 200
 
 #get single teacher
 @app.route('/api/teachers/<teacher_id>', methods = ['GET'])
 def get_single_teacher(teacher_id):
     result = single_teacher(teacher_id)
-    return jsonify(result)
+    return jsonify(result), 200
 
 #getting all students for a specific subject or specific session
 @app.route('/api/subjects/<subject_code>/students', methods = ['GET'])
@@ -52,7 +53,7 @@ def get_students_subject(subject_code):
     day = request.args.get('day')
     time = request.args.get('time')
     result = students_subject(subject_code, day, time)
-    return jsonify(result)
+    return jsonify(result), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
