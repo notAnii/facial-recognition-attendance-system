8c75c01a4422f9a1f2e2fd1b9666ccc92e35b11e
notAnii <animazingtoho@gmail.com>
2023-02-05 21:42:38 +0400

    fix: time format
    Adjust time format on /classes route from hh:mm:ss to hh:mm

--- backend/api/teacher/read.py
+++ backend/api/teacher/read.py
@@ -17,12 +17,13 @@ def single_teacher(teacher_id):
 #get all classes for a teacher
 def all_classes(teacher_id):
     db = DBHelper()
+    time_format = str("%H:%i")
     sql = '''
         SELECT Subject.subject_code, Subject.subject_name, Session.day,
-        CAST(Session.start_time AS char) AS start_time , CAST(Session.end_time AS char) AS end_time,
+        CAST(time_format(Session.end_time,'%s') AS Char) AS start_time , CAST(time_format(Session.end_time,'%s') AS Char) AS end_time,
         Session.room, Session.class_type
         FROM Subject, Session
         WHERE Subject.subject_code = Session.subject_code AND teacher_id = '%s' 
-        ''' % teacher_id
+        ''' % (time_format, time_format, teacher_id)
     result = db.fetch(sql)
     return result
