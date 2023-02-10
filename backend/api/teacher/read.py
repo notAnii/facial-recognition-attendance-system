f6641ca678599648dc641e8ebf5af077b994e942
notAnii <animazingtoho@gmail.com>
2023-02-10 19:35:38 +0400

    fix: start time issue on classes route
    Fix issue on /classes route where start time was showing end time

--- backend/api/teacher/read.py
+++ backend/api/teacher/read.py
@@ -20,7 +20,7 @@ def all_classes(teacher_id):
     time_format = str("%H:%i")
     sql = '''
         SELECT Subject.subject_code, Subject.subject_name, Session.day,
-        CAST(time_format(Session.end_time,'%s') AS Char) AS start_time , CAST(time_format(Session.end_time,'%s') AS Char) AS end_time,
+        CAST(time_format(Session.start_time,'%s') AS Char) AS start_time , CAST(time_format(Session.end_time,'%s') AS Char) AS end_time,
         Session.room, Session.class_type
         FROM Subject, Session
         WHERE Subject.subject_code = Session.subject_code AND teacher_id = %s 
