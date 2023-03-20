42092bc166b05d2e4b1fde1e1a42fb66a2ab8413
LizardTheWizrd <yassinakkad172@gmail.com>
2023-03-20 21:21:40 +0400

    When clicking edit teacher or student classes you can now search specific teacher

--- src/components/admin/teacher2/hero.tsx
+++ src/components/admin/teacher2/hero.tsx
@@ -1,31 +1,7 @@
-import {
-  Box,
-  Container,
-  Text,
-  Stack,
-  VStack,
-  Avatar,
-  HStack,
-  Table,
-  Thead,
-  Tbody,
-  Tfoot,
-  Tr,
-  Th,
-  Td,
-  TableCaption,
-  TableContainer,
-  Heading,
-  Center,
-  Input,
-  Button,
-  Select,
-} from "@chakra-ui/react";
+import { Box, Container, Text, VStack, Input, Button } from "@chakra-ui/react";
 import React, { useContext, useEffect, useState } from "react";
 import "@fontsource/open-sans";
-import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons";
-import { BsFilterLeft } from "react-icons/bs";
-import { WeekContext } from "../../context";
+
 import axios from "axios";
 import Link from "next/link";
 import Head from "next/head";
@@ -33,8 +9,9 @@ import Head from "next/head";
 type Props = {};
 
 const Hero = (props: Props) => {
-  const { weekNumber, setWeekNumber } = useContext(WeekContext);
   const [searchQuery, setSearchQuery] = useState("");
+  const [isActive, setIsActive] = useState(false);
+  const [flag, setFlag] = useState(false);
 
   const [data, setData] = useState<
     Array<{
@@ -49,61 +26,21 @@ const Hero = (props: Props) => {
     }>
   >([]);
 
-  const [teacherData, setTeacherData] = useState<
-    Array<{
-      department: string;
-      position: string;
-      teacher_name: string;
-    }>
-  >([]);
-
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios.get("http://127.0.0.1:5000/api/v1/teacher-classes/123", {
-        withCredentials: true,
-      });
-      setData(result.data);
-    };
+    if (flag) {
+      const fetchData = async () => {
+        const result = await axios.get(
+          `http://127.0.0.1:5000/api/v1/teacher-classes/${searchQuery}`,
+          {
+            withCredentials: true,
+          }
+        );
+        setData(result.data);
+      };
 
-    const fetchTeacherData = async () => {
-      const result = await axios.get(
-        "http://127.0.0.1:5000/api/v1/teacher-info",
-        {
-          withCredentials: true,
-        }
-      );
-      setTeacherData(result.data);
-    };
-
-    fetchTeacherData();
-    fetchData();
-  }, [weekNumber]);
-
-  const filteredData = data.filter((item) => {
-    const searchQueryLower = searchQuery.toLowerCase();
-    const subject_codeLower = item.subject_code.toString().toLowerCase();
-    const dayLower = item.day.toString().toLowerCase();
-
-    // Filter out items that match the search query exactly
-    if (subject_codeLower === searchQueryLower) {
-      return true;
+      fetchData();
     }
-
-    if (dayLower === searchQueryLower) {
-      return true;
-    }
-    // Filter out items that start with the search query
-    if (subject_codeLower.startsWith(searchQueryLower)) {
-      return true;
-    }
-
-    if (dayLower.startsWith(searchQueryLower)) {
-      return true;
-    }
-
-    // Filter out items that don't match the search query
-    return item.subject_name.toLowerCase().startsWith(searchQueryLower);
-  });
+  }, [isActive]);
 
   const handleSearch = (e: {
     target: { value: React.SetStateAction<string> };
@@ -111,7 +48,10 @@ const Hero = (props: Props) => {
     setSearchQuery(e.target.value);
   };
 
-
+  const handleClick = () => {
+    setIsActive(!isActive);
+    setFlag(true);
+  };
 
   return (
     <Container
@@ -124,9 +64,9 @@ const Hero = (props: Props) => {
         <title>Edit Teacher Classes</title>
         <link rel="shortcut icon" href="/logo.png" type="image/x-icon" />
       </Head>
-      
+
       <VStack w="100%">
-      <Box //Box Bottom
+        <Box //Box Bottom
           backgroundColor={"white"}
           w="100%"
           maxW="8xl"
@@ -150,10 +90,9 @@ const Hero = (props: Props) => {
           </Text>
 
           <Box paddingLeft={9} paddingTop={8}>
-            <Text
-            fontFamily={"Open Sans"}
-            fontSize={"20px"}
-            >Teacher ID</Text>
+            <Text fontFamily={"Open Sans"} fontSize={"20px"}>
+              Teacher ID
+            </Text>
             <Input
               border={"2px solid black"}
               borderRadius={"xl"}
@@ -161,178 +100,163 @@ const Hero = (props: Props) => {
               onChange={handleSearch}
               value={searchQuery}
             />
-          <Button
-           fontSize={"lg"}
-           borderRadius={14}
-           variant={"ghost"}
-           bgColor={"#818589"}
-           color="white"
-           marginLeft={30}
-           w="40"
-           _hover={{
-           bgColor: "white",
-           color: "#818589",
-           }}
-           >
-            Search
-           </Button>
+            <Button
+              fontSize={"lg"}
+              borderRadius={14}
+              variant={"ghost"}
+              bgColor={"#818589"}
+              color="white"
+              marginLeft={30}
+              w="40"
+              onClick={handleClick}
+              _hover={{
+                bgColor: "white",
+                color: "#818589",
+              }}
+            >
+              Search
+            </Button>
 
-           <Box //Box Bottom
-          backgroundColor={"#F0F0F0"}
-          w="100%"
-          maxW="5xl"
-          m={1}
-          borderRadius={40}
-          border="1px"
-          borderColor="black"
-          overflowY="auto"
-          overflowX="auto"
-          paddingTop={3}
-          marginTop={5}
-        >
-          <Box //Box that holds the table
-          h="sm"
-          borderRadius={10}
-          overflowY="auto"
-          overflowX="auto"
-          sx={{
-            "&::-webkit-scrollbar": {
-              width: "16px",
-              borderRadius: "8px",
-              backgroundColor: `rgba(0, 0, 0, 0.10)`,
-            },
-            "&::-webkit-scrollbar-thumb": {
-              borderRadius: "8px",
-              backgroundColor: `rgba(0, 0, 0, 0.10)`,
-            },
-          }}
-        >
-          <Box
-            paddingLeft="2%"
-            h="10%"
-            display="flex"
-            position="sticky"
-            top={0}
-            bg="#ECECEC"
-            zIndex={1}
-          >
-            <Box w="15%" display="flex" alignItems="center">
-              <Text fontWeight={"bold"}>Subject Code</Text>
-            </Box>
-            <Box w="15%" display="flex" alignItems="center">
-              <Text fontWeight={"bold"}>Subject Name</Text>
-            </Box>
-            <Box w="15%" display="flex" alignItems="center">
-              <Text fontWeight={"bold"}>Day</Text>
-            </Box>
-            <Box w="15%" display="flex" alignItems="center">
-              <Text fontWeight={"bold"}>Start Time</Text>
-            </Box>
-            <Box w="15%" display="flex" alignItems="center">
-              <Text fontWeight={"bold"}>End Time</Text>
-            </Box>
-            <Box w="13%" display="flex" alignItems="center" />
-            <Box w="13%" display="flex" alignItems="center" />
-          </Box>
+            <Box //Box Bottom
+              backgroundColor={"#F0F0F0"}
+              w="100%"
+              maxW="5xl"
+              m={1}
+              borderRadius={40}
+              border="1px"
+              borderColor="black"
+              overflowY="auto"
+              overflowX="auto"
+              paddingTop={3}
+              marginTop={5}
+            >
+              <Box //Box that holds the table
+                h="sm"
+                borderRadius={10}
+                overflowY="auto"
+                overflowX="auto"
+                sx={{
+                  "&::-webkit-scrollbar": {
+                    width: "16px",
+                    borderRadius: "8px",
+                    backgroundColor: `rgba(0, 0, 0, 0.10)`,
+                  },
+                  "&::-webkit-scrollbar-thumb": {
+                    borderRadius: "8px",
+                    backgroundColor: `rgba(0, 0, 0, 0.10)`,
+                  },
+                }}
+              >
+                <Box
+                  paddingLeft="2%"
+                  h="10%"
+                  display="flex"
+                  position="sticky"
+                  top={0}
+                  bg="#ECECEC"
+                  zIndex={1}
+                >
+                  <Box w="15%" display="flex" alignItems="center">
+                    <Text fontWeight={"bold"}>Subject Code</Text>
+                  </Box>
+                  <Box w="15%" display="flex" alignItems="center">
+                    <Text fontWeight={"bold"}>Subject Name</Text>
+                  </Box>
+                  <Box w="15%" display="flex" alignItems="center">
+                    <Text fontWeight={"bold"}>Day</Text>
+                  </Box>
+                  <Box w="15%" display="flex" alignItems="center">
+                    <Text fontWeight={"bold"}>Start Time</Text>
+                  </Box>
+                  <Box w="15%" display="flex" alignItems="center">
+                    <Text fontWeight={"bold"}>End Time</Text>
+                  </Box>
+                  <Box w="13%" display="flex" alignItems="center" />
+                  <Box w="13%" display="flex" alignItems="center" />
+                </Box>
 
-          <>
-            {filteredData.map((item) => (
-              <Box paddingLeft="2%" h="13%" display="flex">
-                <Box w="15%" display="flex" alignItems="center">
-                  <Text>{item.subject_code}</Text>
-                </Box>
-                <Box w="15%" display="flex" alignItems="center">
-                  <Text>{item.subject_name}</Text>
-                </Box>
-                <Box w="15%" display="flex" alignItems="center">
-                  <Text>{item.day}</Text>
-                </Box>
-                <Box w="15%" display="flex" alignItems="center">
-                  <Text>{item.start_time}</Text>
-                </Box>
-                <Box w="15%" display="flex" alignItems="center">
-                  <Text>{item.end_time}</Text>
-                </Box>
-                <Box
-                  w="13%"
-                  display="flex"
-                  justifyContent="center"
-                  alignItems="center"
-                >
-                  <Link
-                    href={{
-                      pathname: "/admin/edit2",
-                      query: {
-                        subjectCode: item.subject_code,
-                        sessionNumber: item.session_number,
-                        day: item.day,
-                        startTime: item.start_time,
-                        endTime: item.end_time,
-                        classType: item.class_type,
-                      },
-                    }}
-                  >
-                    <Button
-                       paddingLeft={6}
-                       paddingRight={6}
-                      fontSize={"sm"}
-                      rounded={"full"}
-                      variant={"ghost"}
-                      bgColor={"#818589"}
-                      color="white"
-                      _hover={{
-                        bgColor: "white",
-                        color: "#818589",
-                      }}
-                    >
-                      Edit
-                    </Button>
-                  </Link>
-                </Box>
-                <Box
-                  w="13%"
-                  display="flex"
-                  justifyContent="center"
-                  alignItems="center"
-                >
-                  <Link
-                    href={{
-                      pathname: "/afterLogin/attend",
-                      query: {
-                        subjectCode: item.subject_code,
-                        sessionNumber: item.session_number,
-                        day: item.day,
-                        startTime: item.start_time,
-                        endTime: item.end_time,
-                        classType: item.class_type,
-                      },
-                    }}
-                  >
-                    <Button
-                      paddingLeft={6}
-                      paddingRight={6}
-                      fontSize={"sm"}
-                      rounded={"full"}
-                      variant={"ghost"}
-                      bgColor={"#818589"}
-                      color="white"
-                      _hover={{
-                        bgColor: "white",
-                        color: "#818589",
-                      }}
-                    >
-                      Delete
-                    </Button>
-                  </Link>
-                </Box>
+                <>
+                  {data.map((item) => (
+                    <Box paddingLeft="2%" h="13%" display="flex">
+                      <Box w="15%" display="flex" alignItems="center">
+                        <Text>{item.subject_code}</Text>
+                      </Box>
+                      <Box w="15%" display="flex" alignItems="center">
+                        <Text>{item.subject_name}</Text>
+                      </Box>
+                      <Box w="15%" display="flex" alignItems="center">
+                        <Text>{item.day}</Text>
+                      </Box>
+                      <Box w="15%" display="flex" alignItems="center">
+                        <Text>{item.start_time}</Text>
+                      </Box>
+                      <Box w="15%" display="flex" alignItems="center">
+                        <Text>{item.end_time}</Text>
+                      </Box>
+                      <Box
+                        w="13%"
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                      >
+                        <Link
+                          href={{
+                            pathname: "/admin/edit2",
+                            query: {
+                              subjectCode: item.subject_code,
+                              sessionNumber: item.session_number,
+                              day: item.day,
+                              startTime: item.start_time,
+                              endTime: item.end_time,
+                              classType: item.class_type,
+                            },
+                          }}
+                        >
+                          <Button
+                            paddingLeft={6}
+                            paddingRight={6}
+                            fontSize={"sm"}
+                            rounded={"full"}
+                            variant={"ghost"}
+                            bgColor={"#818589"}
+                            color="white"
+                            _hover={{
+                              bgColor: "white",
+                              color: "#818589",
+                            }}
+                          >
+                            Edit
+                          </Button>
+                        </Link>
+                      </Box>
+                      <Box
+                        w="13%"
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                      >
+                        <Button
+                          paddingLeft={6}
+                          paddingRight={6}
+                          fontSize={"sm"}
+                          rounded={"full"}
+                          variant={"ghost"}
+                          bgColor={"#818589"}
+                          color="white"
+                          _hover={{
+                            bgColor: "white",
+                            color: "#818589",
+                          }}
+                        >
+                          Delete
+                        </Button>
+                      </Box>
+                    </Box>
+                  ))}
+                </>
               </Box>
-            ))}
-          </>
-        </Box>
-        </Box>
-
+            </Box>
           </Box>
-          
         </Box>
       </VStack>
     </Container>
